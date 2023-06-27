3181458a290b1322f1a4f3251dc111d42cdb7002
Mateo Gómez Fleytas <mateogf@hotmail.com>
2023-06-27 15:35:38 -0300

    deleteamos index.css

--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      
+      <App />
     </ChakraProvider>
-    <App />
   </React.StrictMode>
 );
 
